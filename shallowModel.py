1ee7f6679a014867c3b69b44cfdb73d6458739f0
brschmidt <brschmidt@wpi.edu>
2022-11-22 11:20:27 -0500

    formatting

--- shallowModel.py
+++ shallowModel.py
@@ -15,7 +15,7 @@ if path.exists('XtrDrivingImages.npy') and path.exists('YtrDrivingLabels.npy'):
     # if numpy training files exist, load training files
     train_matrix, train_labels = np.load('XtrDrivingImages.npy') / 255.0, np.load('YtrDrivingLabels.npy')
 
-    #remove this
+    # remove this
     # idxs = np.random.permutation(train_matrix.shape[0])[0:size]
     # train_matrix, train_labels = train_matrix[idxs,:], train_labels[idxs]
 
@@ -23,12 +23,12 @@ if path.exists('XteDrivingImages.npy') and path.exists('XteDrivingImageNames.npy
     # if numpy testing files exist, load testing files
     test_matrix, test_names = np.load('XteDrivingImages.npy'), np.load('XteDrivingImageNames.npy')
 
-    #remove this unless u
+    # remove this unless u
     # idxs = np.random.permutation(test_matrix.shape[0])[0:size]
     # test_matrix, test_names = test_matrix[idxs,:] / 255.0, test_names[idxs]
 
-
-print("Min:{}, Max: {}".format(np.min(train_matrix), np.max(train_matrix)))  # ensure values are normalized (between 0 and 1)
+print("Min:{}, Max: {}".format(np.min(train_matrix),
+                               np.max(train_matrix)))  # ensure values are normalized (between 0 and 1)
 
 n, m = train_matrix.shape[0], train_matrix.shape[1]
 xtrain_flat = train_matrix.reshape(-1, m ** 2)
@@ -43,9 +43,8 @@ x_te = scaler.transform(xtest_flat)
 
 print("scaling done\nstart training")
 
-softReg = LogisticRegression(multi_class = 'multinomial', solver = 'sag')
+softReg = LogisticRegression(multi_class='multinomial', solver='sag')
 softReg.fit(x_tr, train_labels)
-
 
 coeffs = softReg.coef_
 print('co', coeffs.shape)
@@ -58,4 +57,5 @@ print('pred', predictions[0])
 
 output = np.vstack((test_names, predictions.transpose())).transpose()
 
-pd.DataFrame(output).to_csv("solution.csv", index=None, header=['img','c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'])
+pd.DataFrame(output).to_csv("solution.csv", index=None,
+                            header=['img', 'c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'])
